dac444cc06931fabbbc38d618458920b1591e972
mochammadsufandi <mochammaddwi09@gmail.com>
2024-02-09 23:19:18 +0700

    add variables to DOM manipulation and eventlistener for changing to dark-theme

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const menu = document.querySelector('.hamburger-menu');
 const navbar = document.querySelector('.navbar');
+const themeSwitch = document.getElementById('theme-switch');
+const body = document.querySelector('body');
+const spanFeather = document.getElementById('span-feather');
+const mainContainer = document.querySelector('.main-container');
+const contentBox = document.querySelectorAll('.content');
+const date = document.querySelectorAll('.date');
+const writer = document.querySelectorAll('.writer');
 
 // Active and Close Sidebar menu for Navbar using hamburger menu
 menu.addEventListener('click',() => {
@@ -10,6 +17,39 @@ menu.addEventListener('click',() => {
 document.addEventListener('click',(ev) => {
     if (!menu.contains(ev.target) && !navbar.contains(ev.target)) {
         navbar.classList.remove('active');
+    }
+})
+
+// Active and De-active dark-theme 
+themeSwitch.addEventListener('click', () => {
+    if (themeSwitch.checked === false) {
+        body.classList.add('bg-dark');
+        spanFeather.classList.add('dark-feather');
+        mainContainer.classList.add('dark-main-container');
+        for (let i=0; i<contentBox.length; i++) {
+            const h2 = contentBox[i].querySelector('h2');
+            const p = contentBox[i].querySelector('.content-paragraph');
+            contentBox[i].classList.add('bg-content');
+            h2.classList.add('dark-text');
+            date[i].classList.add('dark-text-date');
+            writer[i].classList.add('dark-text-date');
+            p.classList.add('dark-text');
+        }
+        
+    } else {
+        body.classList.remove('bg-dark');
+        spanFeather.classList.remove('dark-feather');
+        mainContainer.classList.remove('dark-main-container');
+        for (let i=0; i<contentBox.length; i++) {
+            const h2 = contentBox[i].querySelector('h2');
+            const p = contentBox[i].querySelector('.content-paragraph');
+            contentBox[i].classList.remove('bg-content');
+            h2.classList.remove('dark-text');
+            date[i].classList.remove('dark-text');
+            writer[i].classList.remove('dark-text-date');
+            p.classList.remove('dark-text');
+        }
+
     }
 })
 
